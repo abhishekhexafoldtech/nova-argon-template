21003609561e53fefecfd210f0964466defd5477
dattahexafold <dattatraya.ghadage@hexafoldtech.com>
2023-07-03 18:16:27 +0530

    added phone number component

---
+++ components/Phone/PhoneNumber.vue
@@ -0,0 +1,21 @@
+<template> 
+    <MazPhoneNumberInput
+      v-model="phoneNumber"
+      show-code-on-list
+      color="info"
+      :preferred-countries="['IN']"
+      :ignored-countries="['AC']"
+      @update="results = $event"
+      :success="results?.isValid"
+    />
+    <code>
+      {{ results }}
+    </code>
+  </template>
+  
+  <script setup>
+    import MazPhoneNumberInput from 'maz-ui/components/MazPhoneNumberInput' 
+    import { ref } from 'vue'
+    const phoneNumber = ref()
+    const results = ref()
+  </script>
