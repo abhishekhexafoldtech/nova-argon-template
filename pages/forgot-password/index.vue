ccff164a5048863cf8e3caccb014123dcdb75529
Abhishek Anand <136606965+abhishekhexafoldtech@users.noreply.github.com>
2023-07-04 15:04:53 +0530

    update login and forgot password

--- pages/forgot-password/index.vue
+++ pages/forgot-password/index.vue
@@ -7,28 +7,50 @@
           <div class="container">
             <div class="login-form text-center">
               <center><img src="@/assets/nova_logo.png" /></center>
-              <br>
+              <br />
               <h3>Forgot password</h3>
-              <div style="width:80%;margin-left: 37px;">
-                <small class="text-center">Please enter your phone number to receive a verification code </small>
+              <div style="width: 80%; margin-left: 37px">
+                <small class="text-center"
+                  >Please enter your phone number to receive a verification code
+                </small>
               </div>
-              <br>
-              <br>
+              <br />
+              <br />
               <form @submit.prevent="handleSubmit">
-                <div style="position:absolute;width:max-content;background: white;margin-top: -15px;" class="mx-4"
-                  v-if="inputPlaceHolder">
+                <div
+                  style="
+                    position: absolute;
+                    width: max-content;
+                    background: white;
+                    margin-top: -15px;
+                  "
+                  class="mx-4"
+                  v-if="inputPlaceHolder"
+                >
                   <small>Phone number</small>
                 </div>
                 <div class="">
-                  <input type="text" class="form-control" placeholder="Phone Number" v-model="inputPlaceHolder" />
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Phone Number"
+                    v-model="inputPlaceHolder"
+                  />
                 </div>
                 <div class="forget-error" v-if="!allowNext && inputPlaceHolder">
-                  <small class="text-danger">Please provide registered mobile number</small>
+                  <small class="text-danger"
+                    >Please provide registered mobile number</small
+                  >
                 </div>
                 <div v-if="allowNext">
                   <br />
                 </div>
-                <NuxtLink to="/forgot-password/otp"> <input type="submit" class="btn w-100 btn-primary" value="Next" />
+                <NuxtLink to="/forgot-password/otp">
+                  <input
+                    type="submit"
+                    class="btn w-100 btn-primary"
+                    value="Next"
+                  />
                 </NuxtLink>
               </form>
               <div class="py-5">
@@ -40,9 +62,21 @@
         </div>
       </div>
       <div>
-        <div class="image p-5"
-          style="background: linear-gradient(124deg, rgba(122, 165, 191, 0.6) -1.8%, rgba(69, 105, 135, 0.6) 105.23%);">
-          <img src="@/assets/img/sign-in-password.png" width="580" style="margin-top: 5rem;" />
+        <div
+          class="image p-5"
+          style="
+            background: linear-gradient(
+              124deg,
+              rgba(122, 165, 191, 0.6) -1.8%,
+              rgba(69, 105, 135, 0.6) 105.23%
+            );
+          "
+        >
+          <img
+            src="@/assets/img/sign-in-password.png"
+            width="580"
+            style="margin-top: 5rem"
+          />
         </div>
       </div>
     </div>
@@ -51,29 +85,28 @@
 
 
 <script setup>
-
 const inputPlaceHolder = ref("");
 const allowNext = ref(true);
 
 const handleSubmit = () => {
   if (!inputPlaceHolder.value) {
-    return
+    return;
   }
   if (inputPlaceHolder.value === "12345678") {
     allowNext.value = true;
     const router = useRouter();
-    router.push("/forgot-password/otp")
+    router.push("/forgot-password/otp");
   } else {
-    allowNext.value = false
+    allowNext.value = false;
   }
 };
 
 watch(inputPlaceHolder, () => {
   if (inputPlaceHolder.value === "") {
     allowNext.value = true;
-    console.log(allowNext)
+    console.log(allowNext);
   }
-})
+});
 
 definePageMeta({
   layout: "",
@@ -82,13 +115,13 @@ definePageMeta({
 
 
 <style scoped>
-@media screen and (max-width:750px) {
+@media screen and (max-width: 750px) {
   .container {
     width: 100vw;
   }
 }
-@media screen and (max-width:450px) {
-  .login-form{
+@media screen and (max-width: 450px) {
+  .login-form {
     padding: 30px 10px 10px 10px;
   }
 }
