827decd394b6b3d397d2e7b791128b721b0e8d3f
mithunhexafoldtech <mithun.maity@hexafoldtech.com>
2023-07-12 16:18:36 +0530

    Design changes for blank-page in Zone
    Design changes for blank-page in Zone

--- components/BlankPage.vue
+++ components/BlankPage.vue
@@ -1,21 +1,14 @@
 <template>
-  <div class="container main-container">
-    <div class="row main-section">
-      <img
-        src="@/assets/svg/empty_product.svg"
-        class="blank-image w-40 w-md-20"
-        alt="Blank0"
-      />
-      <h4 class="fw-bolder mb-3">No {{ title }} added</h4>
-      <br />
-      <p>You haven't added any {{ title }}.</p>
-      <p>Start by adding first {{ title }}</p>
-      <button
-        class="btn w-70 w-sm-40 w-lg-25 btn-primary mt-3"
-        @click="handleBlankPage"
-      >
-        + {{ buttonText }}
-      </button>
+  <div class="blank_page">
+    <div class="bp_inner">
+      <div class="bp_box">
+        <img src="@/assets/svg/empty_product.svg" alt="Blank0" />
+        <h3>No {{ title }} added</h3>
+        <p>You haven't added any {{ title }}.<br/>Start by adding first {{ title }}</p>
+        <button class="btn w-100 btn-primary" @click="handleBlankPage">
+          + {{ buttonText }}
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -35,27 +28,3 @@ defineProps({
   },
 });
 </script>
-<style scoped>
-.main-container {
-  background-color: #ffff;
-  height: 75vh;
-  width: 98%;
-  overflow: hidden;
-}
-
-.main-section {
-  display: flex;
-  justify-content: center;
-  text-align: center;
-}
-p {
-  line-height: 10px;
-  font-family: Open, Sans;
-}
-@media screen and (min-width: 768px) {
-  .main-container {
-    position: relative;
-    bottom: 55px;
-  }
-}
-</style>
