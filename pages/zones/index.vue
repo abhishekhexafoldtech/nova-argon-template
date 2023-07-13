6eddf05131cd55eb7da0fa52b6d720acb2fc5116
Abhishek Anand <136606965+abhishekhexafoldtech@users.noreply.github.com>
2023-07-13 11:55:42 +0530

    blank page updated

--- pages/zones/index.vue
+++ pages/zones/index.vue
@@ -4,11 +4,13 @@
       title="Product"
       buttonText="Add Product"
       :handleBlankPage="handleBlankPage"
+      :blankImage="productBlankImage"
     />
   </div>
 </template>
 <script setup>
 import BlankPage from "@/components/BlankPage.vue";
+import productBlankImage from "@/assets/svg/empty_product.svg"
 
 //handleBlank Page
 const handleBlankPage = () => {
