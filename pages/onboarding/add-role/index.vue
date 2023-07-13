e154e3444bfaf296fcbe33c2534b1e2e162f7f7c
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-07-13 12:09:11 +0530

    add-roll

--- pages/onboarding/add-role/index.vue
+++ pages/onboarding/add-role/index.vue
@@ -1,8 +1,27 @@
 <template>
-    <div>add Role form</div>
-    <NuxtLink to="/onboarding"><button class="btn btn-primary">Save</button></NuxtLink>
+
+    <section class="add-role-wrap">
+        <div class="bg-white rounded-3" style="margin:50px 20px 20px 20px;height: 98%;">
+            <div class="px-4 py-4">
+
+                <h4>Add new role</h4>
+                <p>Adding new role</p>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+   
 </template>
 
 <script setup>
 
 </script>
+
+<style scoped>
+.add-role-wrap{
+    width:100%;
+    height:85vh;
+}
+</style>
