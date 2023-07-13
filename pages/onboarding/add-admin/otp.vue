cc9a147a675dbf39de9f717015da634c0ee5fe98
Abhishek Anand <136606965+abhishekhexafoldtech@users.noreply.github.com>
2023-07-13 11:07:09 +0530

    layouts for super-admin onboarding flow

---
+++ pages/onboarding/add-admin/otp.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+       <h1>OTP</h1>
+
+
+        <button @click="centerDialogVisible = true" class="btn btn-primary">Next</button>
+
+
+        <el-dialog v-model="centerDialogVisible" title="Warning" width="30%" center>
+            <span>
+            It should be noted that the content will not be aligned in center by
+            default
+            </span>
+            <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="handleWithEmail">send email</el-button>
+
+                <el-button type="primary" @click="handleWithOnboarding">
+                    Continue with onboarding
+                </el-button>
+            </span>
+            </template>
+        </el-dialog>
+
+    </div>
+</template>
+
+<script  setup>
+import { ref } from 'vue'
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const centerDialogVisible = ref(false)
+
+function handleWithEmail() {
+    centerDialogVisible.value = false
+    router.push("/onboarding")
+}
+
+function handleWithOnboarding() {
+    centerDialogVisible.value = false
+    router.push("/onboarding/complete-onboarding")
+}
+
+</script>
+
+
+<style scoped>
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
+</style>
---
+++ pages/onboarding/add-role/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>add Role form</div>
+    <NuxtLink to="/onboarding"><button class="btn btn-primary">Save</button></NuxtLink>
+</template>
+
+<script setup>
+
+</script>
