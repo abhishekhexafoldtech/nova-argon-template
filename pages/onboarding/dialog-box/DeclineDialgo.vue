a865bbf026a4335a6111d8df08ada51da41f9e35
mithunhexafoldtech <mithun.maity@hexafoldtech.com>
2023-07-14 12:51:35 +0530

    Design changes in onboarding approval
    Design changes in onboarding approval

--- pages/onboarding/dialog-box/DeclineDialgo.vue
+++ pages/onboarding/dialog-box/DeclineDialgo.vue
@@ -1,19 +1,18 @@
 <template>
-  <div>
-    <el-dialog v-model="props.dialogVisible" title="Decline admin onboarding request">
+  <el-dialog v-model="props.dialogVisible" class="decline_popup">
+    <div class="pop_inner">
+      <h3>Decline admin onboarding request</h3>
       <el-form label-position="top">
         <el-form-item label="Type a reason for decline admin onboarding request">
-          <el-input type="textarea" v-model="formData.name"  autocomplete="off" />
+          <el-input type="textarea" v-model="formData.name" autocomplete="off" />
         </el-form-item>
       </el-form>
-      <template #footer>
-        <span class="dialog-footer">
-          <button class="btn btn-default" @click="handleCloseDialog">Cancel</button>
-          <button class="btn btn-primary" @click="handleFormSubmit">Send </button>
-        </span>
-      </template>
-    </el-dialog>
-  </div>
+      <div class="pop_footer">
+        <button class="btn btn-default" @click="handleCloseDialog">Cancel</button>
+        <button class="btn btn-primary" @click="handleFormSubmit">Send </button>
+      </div>
+    </div>
+  </el-dialog>
 </template>
 
 <script setup>
@@ -21,34 +20,19 @@
 let emit = defineEmits()
 
 let props = defineProps({
-    dialogVisible: Boolean,
-    formData: {
-        name: String
-    }
+  dialogVisible: Boolean,
+  formData: {
+    name: String
+  }
 })
 
 // close dialog 
 function handleCloseDialog() {
-    emit('childClose')
+  emit('childClose')
 }
 
 // submit data 
 function handleFormSubmit() {
-    emit('getChildFormData', props.formData)
+  emit('getChildFormData', props.formData)
 }
 </script>
-
-<style scoped>
-.el-button--text {
-  margin-right: 15px;
-}
-.el-select {
-  width: 300px;
-}
-.el-input {
-  width: 300px;
-}
-.dialog-footer button:first-child {
-  margin-right: 10px;
-}
-</style>
