66c5fccb4360ec6e293e18fe5dd7e683fc80fb4e
dattahexafold <dattatraya.ghadage@hexafoldtech.com>
2023-07-17 17:02:50 +0530

    add admin and onboarding

--- pages/onboarding/add-admin/index.vue
+++ pages/onboarding/add-admin/index.vue
@@ -1,8 +1,85 @@
 <template>
-    <div>add form</div>
-    <NuxtLink to="add-admin/otp"><button class="btn btn-primary">Next</button></NuxtLink>
-</template>
-
-<script setup>
-
-</script>
+    <div class="container p-4 mt-3">
+      <div class="row bg-white rounded-3 admin-form m-1">
+        <div class="col col-md-4 ms-3 mt-3">
+          <h5>Add new admin</h5>
+          <p>Enter your details</p>
+  
+          <el-form
+            ref="form"
+            label-position="top"
+            label-width="100px"
+            :model="newAdminForm"
+            size="large"
+            :rules="formRules"
+          >
+            <el-form-item label="Name" prop="name">
+              <el-input v-model="newAdminForm.name" placeholder="Name" />
+            </el-form-item>
+  
+            <el-form-item label="Phone number" prop="phone_number">
+              <el-input v-model="newAdminForm.phone_number" placeholder="Phone number" />
+            </el-form-item>
+  
+            <el-form-item label="Email" prop="email">
+              <el-input v-model="newAdminForm.email" placeholder="Email" />
+            </el-form-item>
+          </el-form>
+  
+          <div class="row mt-5">
+            <div class="col-6">
+              <button class="btn border border-primary w-100" @click="handleCancel">Cancel</button>
+            </div>
+            <div class="col-6">
+              <button class="btn btn-primary w-100" @click="handleSubmit">Continue</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { reactive, ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { ElMessage } from 'element-plus'
+  
+  const router = useRouter()
+  
+  const newAdminForm = reactive({
+    name: '',
+    phone_number: '',
+    email: ''
+  })
+  
+  const formRules = {
+    name: [{ required: true, message: 'Please enter your name', trigger: 'blur' }],
+    phone_number: [{ required: true, message: 'Please enter your phone number', trigger: 'blur' }],
+    email: [
+      { required: true, message: 'Please enter your email', trigger: 'blur' },
+      { type: 'email', message: 'Please enter a valid email address', trigger: 'blur' }
+    ]
+  }
+  const form = ref(null)
+  const handleSubmit = () => {
+    form.value.validate((valid) => {
+      if (valid) {
+        console.log(JSON.stringify(newAdminForm))
+        router.push('/onboarding/add-admin/otp')
+      } else {
+        ElMessage.error('Please fill in all the required fields')
+      }
+    })
+  }
+  
+  const handleCancel = () => {
+    router.push('/onboarding')
+  }
+  </script>
+  
+  <style>
+  .admin-form {
+    height: 100vh;
+  }
+  </style>
+  
--- pages/onboarding/index.vue
+++ pages/onboarding/index.vue
@@ -1,242 +1,278 @@
 <template>
-    <div class="container-area">
-      <h4 class="page_heading">Manage admins & roles</h4>
-      <div class="table_header">
-        <el-button 
-          v-if="activeName == 'first'"
-          type="button" 
-          class="btn_add" 
-          @click="handleCreateAdmin()"><span class="text-white"><i>+</i>Add Admin</span>
-        </el-button>
-        <el-button 
-          v-if="activeName == 'second'"
-          type="button" 
-          class="btn_add" 
-          @click="handleCreateRole()"><span class="text-white"><i>+</i>Add Role</span>
-        </el-button>
-        <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-          <el-tab-pane label="Admins" name="first">
-            <div class="table-area">
-              <Table 
-                tableHeading="List Admin" 
-                tableSubHeading="" 
-                :moreActionsVisibility="true" 
-                :addButtonVisibility="false" 
-                :tableConfig="adminTableConfig" 
-                :tableData="showAdminListData" 
-                :tableQuery="listQuery" 
-                @pagination="handlePagination()"
-                @edit="handleEdit($event)" 
-                @delete="handleDelete($event)" 
-                :tableCheckBoxVisibility="true"
-                @multipleSelection="handleMultipleSelection($event)" 
-                @search="handleAdminSearch($event)"
-              />
-            </div>
-          </el-tab-pane>
-          <el-tab-pane label="Roles" name="second">
-            <div class="table-area">
-              <Table 
-                tableHeading="List Role" 
-                tableSubHeading="" 
-                :moreActionsVisibility="true"
-                :addButtonVisibility="false" 
-                :tableConfig="roleTableConfig" 
-                :tableData="showRoleListData" 
-                :tableQuery="listQuery" 
-                @pagination="handlePagination()"
-                @edit="handleEdit($event)" 
-                @delete="handleDelete($event)" 
-                :tableCheckBoxVisibility="true"
-                @multipleSelection="handleMultipleSelection($event)" 
-                @search="hanldeRoleSearch($event)"
-              />
-            </div>
-          </el-tab-pane>
-        </el-tabs>
-      </div>
+  <div class="container-area">
+    <h4 class="page_heading">Manage admins & roles</h4>
+    <div class="table_header">
+      <el-button
+        v-if="activeName == 'first'"
+        type="button"
+        class="btn_add"
+        @click="handleCreateAdmin()"
+        ><span class="text-white"><i>+</i>Add Admin</span>
+      </el-button>
+      <el-button
+        v-if="activeName == 'second'"
+        type="button"
+        class="btn_add"
+        @click="handleCreateRole()"
+        ><span class="text-white"><i>+</i>Add Role</span>
+      </el-button>
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane label="Admins" name="first">
+          <div class="table-area" v-if="false">
+            <Table
+              tableHeading="List Admin"
+              tableSubHeading=""
+              :moreActionsVisibility="true"
+              :addButtonVisibility="false"
+              :tableConfig="tableConfig"
+              :tableData="listData"
+              :tableQuery="listQuery"
+              @pagination="handlePagination()"
+              @edit="handleEdit($event)"
+              @delete="handleDelete($event)"
+              :tableCheckBoxVisibility="true"
+              @multipleSelection="handleMultipleSelection($event)"
+            />
+          </div>
+          <div v-else>
+            <BlankPage
+              mainTitle="Admin not added yet"
+              title=""
+              subTitle="Start by adding first admin"
+              buttonText="Add new admin"
+              :handleBlankPage="handleBlankPage"
+              :blankImage="adminBlankImage"
+            />
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="Roles" name="second">
+          <div class="table-area">
+            <Table
+              tableHeading="List Role"
+              tableSubHeading=""
+              :addButtonVisibility="false"
+              :tableConfig="tableConfig"
+              :tableData="listData"
+              :tableQuery="listQuery"
+              @pagination="handlePagination()"
+              @edit="handleEdit($event)"
+              @delete="handleDelete($event)"
+              :tableCheckBoxVisibility="true"
+              @multipleSelection="handleMultipleSelection($event)"
+            />
+          </div>
+        </el-tab-pane>
+      </el-tabs>
     </div>
-  </template>
-  
-  <script setup>
-  import Table from "@/components/tables/Table.vue";
-  import useToast from "@/composables/useToast";
-  import { useRouter } from "vue-router";
-  const router = useRouter();
-  const activeName = ref("first");
-  let roleListData = [
-    {
-      serial_number:1,
-      role:"Distributor",
-    },
-    {
-      serial_number:2,
-      role:"Delivery agents"
-    },
-    
-  ];
-  let adminTableData = [
-    {
-      id:"01",
-      admin_name:"Yaw Graham",
-      phone_number:"0224465884",
-      user_name:"yawgraham@gmail.com",
-      assigned_role:"-"
-    },
-    {
-      id:"02",
-      admin_name:"Kiran Kumar",
-      phone_number:"02243546584",
-      user_name:"kirankumar@gmail.com",
-      assigned_role:"Agent"
-    },
+  </div>
+</template>
 
-  ];
-  const showRoleListData = ref(roleListData);
-  const showAdminListData = ref(adminTableData);
+<script setup>
+import Table from "@/components/tables/Table.vue";
+import useToast from "@/composables/useToast";
+import { useRouter } from "vue-router";
+import BlankPage from "@/components/BlankPage.vue";
+import adminBlankImage from "@/assets/svg/blank_admin.svg";
 
+const router = useRouter();
+const activeName = ref("first");
+const handleBlankPage = () => {
+  router.push("/onboarding/add-admin")
+};
 
-  let roleTableConfig = reactive([
-    {
-      label:"SR.NO",
-      prop:"serial_number",
-      width:"",
-      sortable:true,
-      className:"redFont"
-    },
-    {
-      label:"ROLE NAME",
-      prop:"role",
-      width:"",
-      sortable:true,
-      className:"redFont"
-    },
-    
-  ])
-  let adminTableConfig = reactive([
-    {
-      label:"ADMIN ID",
-      prop:"id",
-      width:"",
-      sortable:true,
-      className:"redFont"
-    },
-    {
-      label:"ADMIN NAME",
-      prop:"admin_name",
-      width:"",
-      sortable:true,
-      className:"redFont"
-    },
-    {
-      label:"PHOONE NUMBER",
-      prop:"phone_number",
-      width:"",
-      sortable:true,
-      className:"redFont"
-    },
-    {
-      label:"USER NAME",
-      prop:"user_name",
-      width:"",
-      sortable:true,
-      className:"redFont"
-    },
-    {
-      label:"ASSIGNED ROLE",
-      prop:"assigned_role",
-      width:"",
-      sortable:true,
-      className:"redFont"
-    },
-    
-  ]);  
-  let listQuery = {
-    page: 1,
-    limit: 10,
-    search: "",
-    searchJoin: "or",
-    orderBy: "created_at",
-    sortedBy: "desc",
-  };
+let tableConfig = reactive([
+  {
+    label: "Image",
+    prop: "file_list",
+    width: "",
+    sortable: "",
+    className: "redFont",
+  },
+  {
+    label: "Name",
+    prop: "name",
+    width: "",
+    sortable: "sortable",
+    className: "redFont",
+  },
+  {
+    label: "Price",
+    prop: "price",
+    width: "",
+    sortable: "sortable",
+    className: "blueFont",
+  },
+  {
+    label: "Address",
+    prop: "address",
+    width: "",
+    sortable: "",
+    className: "blueFont",
+  },
+]);
 
-  // admin search
+let listData = [
+  {
+    price: "1",
+    name: "Amm",
+    address: "aaa",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "2",
+    name: "Baa",
+    address: "bb",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "3",
+    name: "Caa",
+    address: "cc",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "4",
+    name: "Tom",
+    address: "dd",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "5",
+    name: "Daa",
+    address: "ee",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "6",
+    name: "Tom",
+    address: "ff",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "7",
+    name: "Tom",
+    address: "gg",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "8",
+    name: "Amm",
+    address: "aaa",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "9",
+    name: "Baa",
+    address: "bb",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+  {
+    price: "10",
+    name: "Caa",
+    address: "cc",
+    file_list: [
+      {
+        name: "f1c8b75e3e535a44e93444e47fe2f77e.png",
+        url: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      },
+    ],
+  },
+];
 
-  function handleAdminSearch(data){
-  const filter = adminTableData.filter((e)=>{
-        if(e.admin_name.toLowerCase().includes(data.value.toLowerCase()) || e.phone_number.toLowerCase().includes(data.value.toLowerCase()) || e.user_name.toLowerCase().includes(data.value.toLowerCase()) || e.assigned_role.toLowerCase().includes(data.value.toLowerCase())){
-            return e
-        }
-    })
-    if(filter.length === 0){
-        showAdminListData.value = adminTableData;
-    }else{
-        showAdminListData.value = filter;
-    }
-  }
+let listQuery = {
+  page: 1,
+  limit: 10,
+  search: "",
+  searchJoin: "or",
+  orderBy: "created_at",
+  sortedBy: "desc",
+};
 
-  // role search
+// pagination
+function handlePagination(data) {
+  getList();
+}
 
-  function hanldeRoleSearch(data){
-    const filter = roleListData.filter((e)=>{
-        if(e.role.toLowerCase().includes(data.value.toLowerCase())){
-            return e
-        }
-    })
-    if(filter.length === 0){
-        showRoleListData.value = roleListData;
-    }else{
-        showRoleListData.value = filter;
-    }
-  }
-  
-  // pagination
-  function handlePagination(data) {
-    getList();
-  }
-  
-  // add Admin
-  function handleCreateAdmin() {
-    let r = "add-admin";
-    router.push(`onboarding/${r}`);
-  }
+// add Admin
+function handleCreateAdmin() {
+  let r = "add-admin";
+  router.push(`onboarding/${r}`);
+}
 
-  // add Role
-  function handleCreateRole() {
-    let r = "add-role";
-    router.push(`onboarding/${r}`);
-  }
-  
-  // edit
-  function handleEdit(data) {
-    console.log(data);
-    let r = "edit";
-    router.push(`product/${r}-${data.price}`);
-  }
-  
-  // delete
-  function handleDelete(data) {
-    useToast("warning", "You are not allowed to change data of default user");
-    console.log(data);
-  }
-  
-  // get list
-  function getList() {
-    // API CALL MADE
-  }
-  
-  function handleMultipleSelection(data) {
-    console.log(data);
-  }
+// add Role
+function handleCreateRole() {
+  let r = "add-role";
+  router.push(`onboarding/${r}`);
+}
 
-  onMounted(()=>{
-    const route = useRouter();
-    if(route.previousRoute && route.previousRoute === 'role'){
-      activeName.value = 'second'
-    }
-  })
-  
-  definePageMeta({
-    layout: "default",
-  });
-  </script>
+// edit
+function handleEdit(data) {
+  console.log(data);
+  let r = "edit";
+  router.push(`product/${r}-${data.price}`);
+}
+
+// delete
+function handleDelete(data) {
+  useToast("warning", "You are not allowed to change data of default user");
+  console.log(data);
+}
+
+// get list
+function getList() {
+  // API CALL MADE
+}
+
+function handleMultipleSelection(data) {
+  console.log(data);
+}
+
+definePageMeta({
+  layout: "default",
+});
+</script>
