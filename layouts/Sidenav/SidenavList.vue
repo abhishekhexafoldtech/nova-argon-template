364ba8d1996425e7d32cc83f7cfce6f3df760aad
mithunhexafoldtech <mithun.maity@hexafoldtech.com>
2023-07-24 14:55:41 +0530

    Design changes in Sidenav.
    Design changes in Sidenav.

--- layouts/Sidenav/SidenavList.vue
+++ layouts/Sidenav/SidenavList.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="sidenav-collapse-main" class="collapse navbar-collapse">
+    <h4 class="menu_title">Basics</h4>
     <ul class="navbar-nav">
       <li class="nav-item">
         <SidenavItem to="/dashboards" text="Dashboards" icon="ri-dashboard-line"/>
@@ -32,34 +33,19 @@
       <li class="nav-item">
         <SidenavItem to="/zones" text="Zones" icon="ri-map-pin-5-line" />
       </li>
+    </ul>
+
+    <h4 class="menu_title">Operations</h4>
+    <ul class="navbar-nav">
       <li class="nav-item">
         <SidenavItem to="/dashboards" text="Transactions" icon="ri-secure-payment-line"/>
       </li>
       <li class="nav-item">
         <SidenavItem to="/dashboards" text="Reports" icon="ri-file-chart-line"/>
       </li>
-
-
-      <!-- <li class="mt-3 nav-item">
-        <h6
-          class="text-xs ps-4 text-uppercase font-weight-bolder opacity-6"
-          :class="isRTL ? 'me-4' : 'ms-2'"
-        >
-          PAGES
-        </h6>
-      </li>
-   
-      <li class="nav-item d-flex align-items-center">
-        <i class="ni ni-align-left-2 text-info text-sm opacity-10"></i>
-        <SidenavItem to="/applications/data-tables" text="Data Tables" />
-      </li> -->
-
-
       <li class="nav-item">
         <SidenavItem to="/dashboards" text="Settings" icon="ri-settings-5-line"/>
       </li>
-
-
     </ul>
   </div>
 </template>
--- layouts/Sidenav/index.vue
+++ layouts/Sidenav/index.vue
@@ -8,34 +8,39 @@
         aria-hidden="true"
       ></i> -->
       <NuxtLink class="navbar-brand" to="/">
-        <img src="../../assets/nova_logo_white.png" class="navbar-brand-img " alt="main_logo" />
+        <img src="../../assets/nova_logo2.png" class="navbar-brand-img " alt="main_logo" />
 
         <span class="ms-2 font-weight-bold h6" :class="navStore.sidenavType === 'bg-default bg-transparent' && 'text-white'
           "></span>
       </NuxtLink>
+      <NuxtLink class="nav_admin" to="/">
+        Admin
+      </NuxtLink>
     </div>
-    <hr class="mt-0 horizontal dark" />
+
     <SidenavList />
 
     <div class="mt-3 nav-item">
       <hr class="mt-0 horizontal dark" />
     </div>
     <div class="bootom_nav">
-      <ul>
-        <li>
-          <NuxtLink to="/">
-            <i class="ri-logout-box-r-line"></i>
-            <span>Sign Out</span>
-          </NuxtLink>
-        </li>
-      </ul>
+      <div class="bn_left">
+        <img src="../../assets/img/team-2.jpg" alt="">
+        <div class="bn_left_con">
+          <h5>John Doe</h5>
+          <p>johndoe@gmail.com</p>
+        </div>
+      </div>
+      <NuxtLink class="setting_btn" to="/">
+        <i class="ri-settings-4-fill"></i>
+      </NuxtLink>
     </div>
   </aside>
 </template>
 
 <script setup>
 import SidenavList from "./SidenavList.vue";
-import logo from "@/assets/nova_logo.png";
+import logo from "@/assets/nova_logo2.png";
 import { useNavStore } from "@/stores/NavStore";
 const navStore = useNavStore();
 </script>
