2b88971ea09a76b6bd0b72187df9592c51511144
Abhishek Anand <abhishek.anand@hexafoldtech.com>
2023-08-10 13:03:47 +0530

    add distributor

---
+++ pages/distributor/add-distributor/phase-three.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="pt-5">
+        <h1>phase three</h1>
+    </div>
+</template>
+
+<script setup>
+
+
+
+definePageMeta({
+  layout: "default",
+});
+</script>
---
+++ pages/distributor/add-distributor/phase-two.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="pt-5">
+        <h1>phase two</h1>
+        <el-button @click="handleSubmit">submit</el-button>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+function handleSubmit() {
+    let r = "phase-three";
+    router.push(`${r}`);
+}
+
+definePageMeta({
+  layout: "default",
+});
+</script>
