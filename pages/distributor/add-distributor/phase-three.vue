a6e76d2c2e8a384ee5c50169f0c235125e52f692
mithunhexafoldtech <mithun.maity@hexafoldtech.com>
2023-08-11 17:04:03 +0530

    Design changes in add-distributor-phase3
    Design changes in add-distributor-phase3

--- pages/distributor/add-distributor/phase-three.vue
+++ pages/distributor/add-distributor/phase-three.vue
@@ -1,76 +1,70 @@
 <template>
-  <section class="edit_mang_wrap">
-    <div class="mang_inner comp_inner">
-      <div>
-        <h4>Phase 3 details</h4>
-      </div>
+  <section class="container-area">
+    <h4 class="page_heading mb-3 pb-2">Phase 3 details</h4>
+    <div class="comp_inner">
       <el-form label-position="top" label-width="100px" :model="formData" :rules="formValidationRules" ref="formRef">
-        <div class="">
-          <h6>Insurance & banking details</h6>
-          <el-row>
-            <el-col :xs="15" :sm="10" :md="10" :lg="10" :xl="10">
-              <span>Insurance details</span>
-              <div class="me-md-4">
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Name of Insurance Company" prop="insurence_detail.name_of_insurence_company">
-                    <el-input v-model="formData.insurence_detail.name_of_insurence_company
-                      " placeholder="Name of insurance company" />
-                  </el-form-item>
-                </div>
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Policy Number" prop="insurence_detail.policy_number">
-                    <el-input v-model="formData.insurence_detail.policy_number" placeholder="Policy number" />
-                  </el-form-item>
-                </div>
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Expiry of Insurance" prop="insurence_detail.expiry_of_insurence">
-                    <el-input v-model="formData.insurence_detail.expiry_of_insurence" placeholder="Expiry of insurance" />
-                  </el-form-item>
-                </div>
-                <div class="comp_form_box w355 mt-5">
-                  <div class="fieldrow">
-                    <p>Insurance Card</p>
-                    <SingleFileUpload iconClass="iconClass" @getImage="getInsurenceCard" />
-                  </div>
-                  <p v-if="isUploaded" class="text-danger">
-                    Please upload insurance card
-                  </p>
-                </div>
+        <el-row :gutter="30">
+          <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+            <div class="form_inner_half mb-4">
+              <h5 class="half_subtitle">Insurance details</h5>
+              <div class="fieldrow">
+                <el-form-item label="Name of Insurance Company" prop="insurence_detail.name_of_insurence_company">
+                  <el-input v-model="formData.insurence_detail.name_of_insurence_company
+                    " placeholder="Name of insurance company" />
+                </el-form-item>
               </div>
-            </el-col>
-
-            <el-col :xs="15" :sm="10" :md="10" :lg="10" :xl="10">
-              <div class="ms-md-5">
-                <span>Banking details</span>
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Name of Bank" prop="bank_details.name_of_bank">
-                    <el-input v-model="formData.bank_details.name_of_bank" placeholder="Name of bank" />
-                  </el-form-item>
-                </div>
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Branch Name" prop="bank_details.branch_name">
-                    <el-input v-model="formData.bank_details.branch_name" placeholder="Branch name" />
-                  </el-form-item>
-                </div>
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Branch Code" prop="bank_details.branch_code">
-                    <el-input v-model="formData.bank_details.branch_code" placeholder="Branch code" />
-                  </el-form-item>
-                </div>
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Account Name" prop="bank_details.account_name">
-                    <el-input v-model="formData.bank_details.account_name" placeholder="Account name" />
-                  </el-form-item>
-                </div>
-                <div class="fieldrow mt-4">
-                  <el-form-item label="Account Number" prop="bank_details.account_number">
-                    <el-input v-model="formData.bank_details.account_number" placeholder="Account number" />
-                  </el-form-item>
-                </div>
+              <div class="fieldrow">
+                <el-form-item label="Policy Number" prop="insurence_detail.policy_number">
+                  <el-input v-model="formData.insurence_detail.policy_number" placeholder="Policy number" />
+                </el-form-item>
               </div>
-            </el-col>
-          </el-row>
-        </div>
+              <div class="fieldrow">
+                <el-form-item label="Expiry of Insurance" prop="insurence_detail.expiry_of_insurence">
+                  <el-input v-model="formData.insurence_detail.expiry_of_insurence" placeholder="Expiry of insurance" />
+                </el-form-item>
+              </div>
+              <div class="comp_form_box w355 m-0">
+                <div class="fieldrow m-0">
+                  <p class="frm_label">Insurance Card</p>
+                  <SingleFileUpload iconClass="iconClass" @getImage="getInsurenceCard" />
+                </div>
+                <p v-if="isUploaded" class="text-danger">
+                  Please upload insurance card
+                </p>
+              </div>
+            </div>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+            <div class="form_inner_half">
+              <h5 class="half_subtitle">Banking details</h5>
+              <div class="fieldrow">
+                <el-form-item label="Name of Bank" prop="bank_details.name_of_bank">
+                  <el-input v-model="formData.bank_details.name_of_bank" placeholder="Name of bank" />
+                </el-form-item>
+              </div>
+              <div class="fieldrow">
+                <el-form-item label="Branch Name" prop="bank_details.branch_name">
+                  <el-input v-model="formData.bank_details.branch_name" placeholder="Branch name" />
+                </el-form-item>
+              </div>
+              <div class="fieldrow">
+                <el-form-item label="Branch Code" prop="bank_details.branch_code">
+                  <el-input v-model="formData.bank_details.branch_code" placeholder="Branch code" />
+                </el-form-item>
+              </div>
+              <div class="fieldrow">
+                <el-form-item label="Account Name" prop="bank_details.account_name">
+                  <el-input v-model="formData.bank_details.account_name" placeholder="Account name" />
+                </el-form-item>
+              </div>
+              <div class="fieldrow">
+                <el-form-item label="Account Number" prop="bank_details.account_number">
+                  <el-input v-model="formData.bank_details.account_number" placeholder="Account number" />
+                </el-form-item>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
       </el-form>
       <div class="comp_footer">
         <nuxt-link class="btn btn-default" to="/distributor">Cancel</nuxt-link>
