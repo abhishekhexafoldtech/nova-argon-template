333f8ba4fd266ef6325a597beaa6d99b3402971f
mithunhexafoldtech <mithun.maity@hexafoldtech.com>
2023-08-29 16:50:07 +0530

    Design changes in Multiple File Upload UI

--- components/upload/MultiFileUpload.vue
+++ components/upload/MultiFileUpload.vue
@@ -1,6 +1,14 @@
 <template>
   <el-row class="pr_img_upload_box">
-    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" v-for="box in boxes" :key="box.id">
+    <el-col
+      :xs="24"
+      :sm="24"
+      :md="12"
+      :lg="12"
+      :xl="12"
+      v-for="box in boxes"
+      :key="box.id"
+    >
       <label
         :class="[
           'image-box',
@@ -13,18 +21,25 @@
             class="avatar-uploader"
             :ref="`fileInput${box.id}`"
             :show-file-list="false"
-            @change="file => handleFileChange(file, box)"
+            @change="(file) => handleFileChange(file, box)"
           >
-            <img class="img-fluid overflow-hidden" v-if="box.image" :src="box.image" alt="Preview" />
+            <img
+              class="img-fluid overflow-hidden"
+              v-if="box.image"
+              :src="box.image"
+              alt="Preview"
+            />
             <div class="box_inner" v-else>
               <div class="text">Drop files here to upload</div>
             </div>
           </el-upload>
         </div>
       </label>
-      <div class="file-info" v-if="box.image">
-        {{ box.uploadedFileName.toString().slice(0, 20) }}...
-        <span class="remove-icon" @click="removeFile(box)"><el-icon><CircleClose /></el-icon></span>
+      <div class="file_info" v-if="box.image">
+        <span class="remove_icon" @click="removeFile(box)">
+          <i class="ri-close-line"></i>
+        </span>
+        <p>{{ box.uploadedFileName }}</p>
         <!-- <span class="remove-icon text-success" @click="removeFile(box)"><el-icon><CircleCheck /></el-icon></span> -->
       </div>
     </el-col>
@@ -33,8 +48,8 @@
 
 <script setup>
 import { ref, defineEmits } from "vue";
-import { ElUpload } from 'element-plus';
-import { Plus,CircleClose ,CircleCheck} from '@element-plus/icons-vue'
+import { ElUpload } from "element-plus";
+import { Plus, CircleClose, CircleCheck } from "@element-plus/icons-vue";
 const emit = defineEmits();
 
 const boxes = ref([
@@ -45,7 +60,7 @@ const boxes = ref([
 ]);
 
 const handleFileChange = (file, box) => {
-  if (file.status === 'success') {
+  if (file.status === "success") {
     const uploadedFile = file.raw;
     const reader = new FileReader();
     reader.onload = (e) => {
@@ -62,31 +77,53 @@ const handleFileChange = (file, box) => {
 };
 
 const removeFile = (box) => {
-  emit("removeAvtar",box.image)
- setTimeout(() => {
-  box.image= null;
-  box.uploadedFileName = null;
- }, 100);
+  emit("removeAvtar", box.image);
+  setTimeout(() => {
+    box.image = null;
+    box.uploadedFileName = null;
+  }, 100);
 };
 </script>
 <style>
-.el-upload__input{
- display: none !important;
+.el-upload__input {
+  display: none !important;
 }
 </style>
-<style scoped >
-
-
-.file-info {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+<style scoped lang="scss">
+.file_info {
   margin-top: 10px;
-}
+  position: relative;
 
-.remove-icon {
-  cursor: pointer;
-  color: red;
+  p {
+    color: var(--black) !important;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 18px;
+    margin: 0;
+    padding-left: 35px;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    padding-top: 3px;
+  }
+
+  .remove_icon {
+    font-size: 14px;
+    display: block;
+    background-color: var(--violet);
+    color: white !important;
+    width: 22px;
+    height: 22px;
+    line-height: 22px;
+    text-align: center;
+    border-radius: 50%;
+    cursor: pointer;
+    position: absolute;
+    padding-left: 1px;
+    top: 0;
+    left: 0;
+  }
 }
 
 .file-upload-wrapper {
