f9f2fe8f3a5671138a65e96203e66bcb67d2a9a5
mithunhexafoldtech <mithun.maity@hexafoldtech.com>
2023-09-01 17:55:08 +0530

    Design changes in blank-customer and customer-details-edit

---
+++ pages/customer/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <EmptyManager :icon="CustomerEmptyImage" heading="No customer added yet" description="List of customers not added yet."/>
+    </div>
+</template>
+<script setup>
+import EmptyManager from "@/components/cards/EmptyManager.vue";
+import CustomerEmptyImage from "@/assets/svg/customers_empty.svg"
+</script>
