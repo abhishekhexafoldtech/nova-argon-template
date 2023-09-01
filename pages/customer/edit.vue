f027710500f5bed4d0d6d0c8833a5528414ed796
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-09-01 14:05:57 +0530

    customer-edit-form

---
+++ pages/customer/edit.vue
@@ -0,0 +1,17 @@
+<template>
+    <CustomerEditForm @handleUpdateCustomerDetails="handleUpdate" @handleCloseCustomerEditForm="handleClose"/>
+</template>
+
+<script setup>
+import CustomerEditForm from "@/components/Customer/EditForm.vue";
+function handleUpdate(data){
+    console.log(data)
+}
+function handleClose(data){
+    if(data){
+        console.log("Cancel");
+    }else {
+        console.log("close")
+    }
+}
+</script>
