3e408894cdba6abce08b0f41805195141bc16e9d
mithunhexafoldtech <mithun.maity@hexafoldtech.com>
2023-09-06 14:10:26 +0530

    Design changes in customer-personal-details-ui
    Design changes in customer-personal-details-ui

--- components/buttons/ActiveButton.vue
+++ components/buttons/ActiveButton.vue
@@ -1,31 +1,30 @@
 <template>
-    <div>
-        <div  class="active-button px-4" type=""> <span v-if="isActive"  class="dot"></span>Active</div>
-    </div>
+    <div class="active_btn"><span v-if="isActive" class="dot"></span>Active</div>
 </template>
 <script setup>
-let props=defineProps({
-    isActive:{
-        type:String,
-        default:false
+let props = defineProps({
+    isActive: {
+        type: String,
+        default: false
     }
 })
 </script>
 <style scoped>
-/* active button */
-.dot {
-  display: inline-block;
-  width: 8px;
-  height: 8px;
-  background-color: #67c23a; /* Green color */
-  border-radius: 50%; /* Make it a circle */
-  margin-right: 8px; /* Adjust spacing between dot and text */
+.active_btn {
+    background-color: var(--white);
+    color: var(--black);
+    font-size: 14px;
+    font-weight: 400;
+    padding: 6px 20px;
+    border-radius: 50px;
 }
-.active-button {
- background-color: white;
- border-radius: 18px;
-  display: inline-flex;
-  align-items: center;
-  padding: 5px 10px;
+
+span {
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    background-color: #67c23a;
+    border-radius: 50%;
+    margin-right: 8px;
 }
 </style>
